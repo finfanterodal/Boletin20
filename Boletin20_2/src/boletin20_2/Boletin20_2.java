4dca98f9e4e698fcb6efbfa62f676797d6b74694
Fran <Fran@DESKTOP-8HL9EF5>
2019-03-10 23:00:53 +0100

    Init.

---
+++ Boletin20_2/src/boletin20_2/Boletin20_2.java
@@ -0,0 +1,17 @@
+package boletin20_2;
+
+/**
+ *
+ * @author Fran
+ */
+public class Boletin20_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Tartaglia triangulo = new Tartaglia();
+        triangulo.triangulo(10);
+    }
+
+}
---
+++ Boletin20_2/src/boletin20_2/Tartaglia.java
@@ -0,0 +1,25 @@
+package boletin20_2;
+
+/**
+ *
+ * @author Fran
+ */
+public class Tartaglia {
+
+    public void triangulo(int filas) {
+        int[] a = new int[1];
+        for (int i = 1; i <= filas; i++) {
+            int[] x = new int[i];
+            for (int j = 0; j < i; j++) {
+                if (j == 0 || j == (i - 1)) {
+                    x[j] = 1;
+                } else {
+                    x[j] = a[j] + a[j - 1];
+                }
+                System.out.print(x[j] + " ");
+            }
+            a = x;
+            System.out.println();
+        }
+    }
+}
